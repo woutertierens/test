0d037a620ed29c7cdfbd9d94a12fdba484099dac
trepidacious <trepidacious@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2009-01-29 11:37:19 +0000

    Added to note contract in javadocs to emphasise that calculation results MUST be immutable.

--- jpropeller/src/org/jpropeller/properties/calculated/PropCalculation.java
+++ jpropeller/src/org/jpropeller/properties/calculated/PropCalculation.java
@@ -3,11 +3,24 @@ package org.jpropeller.properties.calculated;
 import java.util.Set;
 
 import org.jpropeller.properties.GeneralProp;
+import org.jpropeller.properties.change.Changeable;
 
 /**
  * A calculation based on a set of Props
- * @author shingoki
- *
+ * 
+ * NOTE: {@link PropCalculation}s MUST produce
+ * values that are immutable (in the sense that
+ * they never change state in any detectable way after
+ * creation). This is because the results of calculations
+ * are considered to be transient - they can always be 
+ * recreated by recalculating based on the same inputs.
+ * If a mutable result is produced, and it is mutated,
+ * then any state in it will be lost whenever the
+ * calculation is repeated. In addition, code using
+ * the results of {@link PropCalculation} will not expect
+ * changes to be made, and so may not listen to results
+ * even if they care {@link Changeable}.
+ * 
  * @param <T>
  * 		The type of value calculated
  */
