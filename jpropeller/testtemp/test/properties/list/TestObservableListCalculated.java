3f53376cb8387f6e46b0e773ce007eac93261747
trepidacious <trepidacious@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2009-01-29 15:31:10 +0000

    * Converted System.out.println()s to logging.  * Made the random test run more times, and as several repeats. Time taken is also tested against a very loose limit, to detect major performance regressions.

--- jpropeller/testtemp/test/properties/list/TestObservableListCalculated.java
+++ jpropeller/testtemp/test/properties/list/TestObservableListCalculated.java
@@ -24,14 +24,18 @@
 package test.properties.list;
 
 
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 
 import org.jpropeller.collection.impl.ObservableListCalculated;
@@ -41,6 +45,7 @@ import org.jpropeller.properties.calculated.PropCalculation;
 import org.jpropeller.properties.list.ListSelectionEditableValueReference;
 import org.jpropeller.properties.list.impl.ListSelectionEditableValueReferenceDefault;
 import org.jpropeller.reference.impl.EditableReferenceToChangeable;
+import org.jpropeller.util.GeneralUtils;
 import org.jpropeller.view.bean.impl.BeanPropListEditor;
 import org.jpropeller.view.table.impl.ListTableView;
 import org.jpropeller.view.table.impl.TableRowViewDirect;
@@ -58,6 +63,8 @@ import com.jgoodies.forms.layout.FormLayout;
  */
 public class TestObservableListCalculated {
 
+	private final static Logger logger = GeneralUtils.logger(TestObservableListCalculated.class);
+	
 	private LotsOfProps a;
 	private LotsOfProps b;
 	private MultiplesListCalc calc;
@@ -87,49 +94,59 @@ public class TestObservableListCalculated {
 		assertList(1, 1);
 		assertList();
 
-		System.out.println();
+		logger.finest("...");
 
-		System.out.println("####Will set a = 2");
+		logger.finest("####Will set a = 2");
 		a.intProp().set(2);
-		System.out.println("####Set a = 2");
+		logger.finest("####Set a = 2");
 		assertList(2, 1);
 		assertList();
 
-		System.out.println();
+		logger.finest("...");
 
-		System.out.println("####Will set a = 1");
+		logger.finest("####Will set a = 1");
 		a.intProp().set(1);
-		System.out.println("####Set a = 1");
-		System.out.println("####Will set b = 2");
+		logger.finest("####Set a = 1");
+		logger.finest("####Will set b = 2");
 		b.intProp().set(2);
-		System.out.println("####Set b = 2");
+		logger.finest("####Set b = 2");
 		
 		assertList(1, 2);
 		assertList();
 
-		System.out.println();
+		logger.finest("...");
 
 		//Thread.sleep(1000);
 		
-		System.out.println("#### - THIS FAILS Will set a = 2");
+		logger.finest("#### - THIS FAILS Will set a = 2");
 		a.intProp().set(2);
-		System.out.println("####Set a = 2");
-		assertList(2, 1);
+		logger.finest("####Set a = 2");
+		assertList(2, 2);
 		assertList();
 
-		System.out.println();
-
+		logger.finest("...");
 		
 		Random r = new Random(0);
 		
-		for (int i = 0; i < 100; i++) {
-			int aVal = r.nextInt(100);
-			int bVal = r.nextInt(100);
-			a.intProp().set(aVal);
-			b.intProp().set(bVal);
-			System.out.println("####i = " + i + ", set a = " + aVal + ", b = " + bVal);
-			assertList(aVal, bVal);
-			assertList();
+		long startTime;
+		
+		
+		for (int repeat = 0; repeat < 20; repeat++) {
+			startTime = System.currentTimeMillis();
+			
+			for (int i = 0; i < 1000; i++) {
+				int aVal = r.nextInt(100);
+				int bVal = r.nextInt(100);
+				a.intProp().set(aVal);
+				b.intProp().set(bVal);
+				logger.finest("####i = " + i + ", set a = " + aVal + ", b = " + bVal);
+				assertList(aVal, bVal);
+				assertList();
+			}
+			
+			long runTime = (System.currentTimeMillis() - startTime);
+			Assert.assertTrue("Testing time for 100 changes < 1000ms", runTime < 200);
+			logger.fine("1000 changes took " + runTime + "ms");
 		}
 	}
 
@@ -138,15 +155,15 @@ public class TestObservableListCalculated {
 		int factor = b.intProp().get();
 		if (length < 0) length = 0;
 
-		System.out.println("Will check length (a) = " + length + ", factor (b) = " + factor);
+		logger.finest("Will check length (a) = " + length + ", factor (b) = " + factor);
 		
 		assertList(length, factor);
 	}
 
 	private void assertList(int length, int factor) {
-		System.out.println("Expecting length = " + length + ", factor = " + factor);
+		logger.finest("Expecting length = " + length + ", factor = " + factor);
 		Assert.assertEquals(length, list.size());
-		System.out.println("Checking contents");
+		logger.finest("Checking contents");
 		
 		for (int i = 0; i < length; i++) {
 			Assert.assertEquals(factor * i, list.get(i));
@@ -178,7 +195,7 @@ public class TestObservableListCalculated {
 			int length = a.get();
 			int factor = b.get();
 			
-			System.out.println("Calculating length = " + length + ", factor = " + factor);
+			logger.finest("Calculating length = " + length + ", factor = " + factor);
 			
 			if (length < 0) length = 0;
 			ArrayList<Integer> list = new ArrayList<Integer>(length);
@@ -206,6 +223,9 @@ public class TestObservableListCalculated {
 	}
 	
 	private static void runDemoApp() {
+		
+		GeneralUtils.enableNimbus();
+		
 		LotsOfProps a = new LotsOfProps();
 		LotsOfProps b = new LotsOfProps();
 		MultiplesListCalc calc = new MultiplesListCalc(a.intProp(), b.intProp());
@@ -213,24 +233,24 @@ public class TestObservableListCalculated {
 		
 		final ListSelectionEditableValueReference<Integer> listRef = new ListSelectionEditableValueReferenceDefault<Integer>(list, Integer.class);
 		ListTableView<Integer> listView = new ListTableView<Integer>(listRef, new TableRowViewDirect<Integer>(Integer.class, "Values"));
-
-		System.out.println(listView.getComponent().getModel().getColumnClass(0));
+		JScrollPane listScroll = new JScrollPane(listView.getComponent());
+		listScroll.setMinimumSize(new Dimension(100, 100));
+		listScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		BeanPropListEditor<LotsOfProps> aEditor = BeanPropListEditor.create(EditableReferenceToChangeable.create(LotsOfProps.class, a));
 		BeanPropListEditor<LotsOfProps> bEditor = BeanPropListEditor.create(EditableReferenceToChangeable.create(LotsOfProps.class, b));
 		
 		//Layout the editors in a frame
 		FormLayout layout = new FormLayout(
-				"fill:200dlu:grow, 5dlu, fill:200dlu:grow", 
-				"fill:200dlu:grow, 5dlu, fill:200dlu:grow");
+				"fill:200dlu:grow, 5dlu, fill:200dlu:grow, 5dlu, fill:200dlu:grow", 
+				"fill:default:grow");
 		
 		DefaultFormBuilder builder = new DefaultFormBuilder(layout);
 		builder.setDefaultDialogBorder();
 		
 		builder.append(aEditor.getComponent());
 		builder.append(bEditor.getComponent());
-		builder.nextRow();
-		builder.append(listView.getComponent());
+		builder.append(listScroll);
 		
 		JFrame frame = new JFrame("Test Calculated List");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
