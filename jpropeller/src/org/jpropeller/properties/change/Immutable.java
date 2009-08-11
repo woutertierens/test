28e04cb466aa7c119475bb6680aa52ba2aaa2a6d
trepidacious <trepidacious@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2009-08-11 13:17:49 +0000

    Moving trunk into tags before replacing by remove-generic-propnames branch

--- jpropeller/src/org/jpropeller/properties/change/Immutable.java
+++
@@ -1,9 +0,0 @@
-package org.jpropeller.properties.change;
-
-/**
- * Marker interface implemented by any class that can GUARANTEE that
- * it does not change in any perceptible way after construction
- * @author shingoki
- */
-public interface Immutable {
-}
