3b3bb6f1cd40ebc4f2ecc7fd257c72476d102f38
trepidacious <trepidacious@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2009-10-30 12:23:23 +0000

    Added row view using a list of keys to determine columns, then looking up values in maps associated with the rows.

---
+++ jpropeller/src/org/jpropeller/view/table/impl/MapRowView.java
@@ -0,0 +1,137 @@
+package org.jpropeller.view.table.impl;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jpropeller.collection.CList;
+import org.jpropeller.properties.Prop;
+import org.jpropeller.properties.change.Change;
+import org.jpropeller.properties.change.ChangeListener;
+import org.jpropeller.properties.change.Changeable;
+import org.jpropeller.transformer.Transformer;
+import org.jpropeller.util.GeneralUtils;
+import org.jpropeller.util.Listeners;
+import org.jpropeller.view.table.TableRowView;
+import org.jpropeller.view.table.TableRowViewListener;
+
+/**
+ * {@link TableRowView} of rows R that each contain a {@link Map}
+ * from K to V.
+ * 
+ * Columns are allocated according to a {@link CList} of K, with each
+ * column showing the mapped values for that key, in the respective row.
+ * 
+ * A {@link Transformer} is used to go from rows of type R to the {@link Map}
+ * for that row.
+ * @param <R>		The type or row 
+ * @param <K> 		The type of key in the maps
+ * @param <V> 		The type of value in the maps
+ */
+public class MapRowView<R, K, V> implements TableRowView<R>, ChangeListener {
+
+	private final static Logger logger = GeneralUtils.logger(MapRowView.class);
+	
+	private final Prop<? extends CList<? extends K>> keys;
+	private final Transformer<? super R, ? extends Map<K, V>> transformer;
+	private final Class<V> vClass;
+	
+	private final Listeners<TableRowViewListener> listeners = new Listeners<TableRowViewListener>();
+	
+	/**
+	 * Make a {@link MapRowView}
+	 * @param vClass	The {@link Class} of value in the maps
+	 * @param keys		The keys for which to display values, this determines
+	 * 					the column list
+	 * @param transformer	The {@link Transformer} from a Row of type R to a {@link Map} from
+	 * 							key (type K) to value (type V)
+	 */
+	public MapRowView(Class<V> vClass, Prop<? extends CList<? extends K>> keys, Transformer<? super R, ? extends Map<K, V>> transformer) {
+		this.vClass = vClass;
+		this.keys = keys;
+		keys.features().addListener(this);
+		this.transformer = transformer;
+	}
+	
+	@Override
+	public void change(List<Changeable> initial, Map<Changeable, Change> changes) {
+		fireChange();
+	}
+	
+	private void fireChange() {
+		for (TableRowViewListener listener : listeners) {
+			listener.tableRowViewChanged(this);
+		}
+	}
+	
+	@Override
+	public Object getColumn(R row, int column) {
+		Map<K, V> map = transformer.transform(row);
+		if (map == null) return null;
+		
+		CList<? extends K> keyList = keys.get();
+		if (keyList == null) return null;
+		
+		if (column < 0 || column > keyList.size()) {
+			return null;
+		}
+		
+		K key = keyList.get(column);
+
+		return map.get(key);
+	}
+
+	@Override
+	public Class<?> getColumnClass(int column) {
+		return vClass;
+	}
+
+	@Override
+	public int getColumnCount() {
+		CList<? extends K> keyList = keys.get();
+		if (keyList == null) {
+			return 0;
+		} else {
+			return keyList.size();
+		}
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		CList<? extends K> keyList = keys.get();
+		if (keyList == null) return null;
+		
+		if (column < 0 || column > keyList.size()) {
+			return null;
+		}
+		
+		return keyList.get(column).toString();
+	}
+
+	@Override
+	public boolean isEditable(R row, int column) {
+		return false;
+	}
+
+	@Override
+	public void setColumn(R row, int column, Object value) {
+	}
+
+	@Override
+	public void addListener(TableRowViewListener listener) {
+		listeners.add(listener);
+	}
+
+	@Override
+	public void removeListener(TableRowViewListener listener) {
+		if (!listeners.remove(listener)) {
+			logger.log(Level.FINE, "Removed TableRowViewListener which was not registered.", new Exception("Stack Trace"));
+		}
+	}
+	
+	@Override
+	public void dispose() {
+		keys.features().removeListener(this);
+	}
+}
