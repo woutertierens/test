2dde916304218c9dfd3e52ec18efd80315875b9d
trepidacious@gmail.com <trepidacious@gmail.com@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2009-11-16 19:39:39 +0000

    Maximum size increased to 10000

--- jpropeller/src/org/jpropeller/view/table/columns/impl/ColumnLayoutDirect.java
+++ jpropeller/src/org/jpropeller/view/table/columns/impl/ColumnLayoutDirect.java
@@ -10,7 +10,7 @@ import org.jpropeller.view.table.columns.ColumnLayout;
 /**
  * The simplest {@link ColumnLayout}, just maps each model column
  * to an output column in standard ordering, with min width 0, max width
- * 1000, preferred width 100.
+ * 10000, preferred width 100.
  */
 public class ColumnLayoutDirect implements ColumnLayout {
 
@@ -19,7 +19,7 @@ public class ColumnLayoutDirect implements ColumnLayout {
 		int columnCount = model.getColumnCount();
 		LinkedList<ColumnInfo> list = new LinkedList<ColumnInfo>();
 		for (int i = 0; i < columnCount; i++) {
-			list.add(new ColumnInfoDefault(i, 100, 0, 1000));
+			list.add(new ColumnInfoDefault(i, 100, 0, 10000));
 		}
 		
 		return list;
