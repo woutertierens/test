c9c7524af115d3f2fd740f6dd5c57c9faaca9327
trepidacious <trepidacious@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2010-02-02 13:24:58 +0000

    * Added support for displaying icons in label view.  * Added editors for Props of Enum and Number.

---
+++ jpropeller/src/org/jpropeller/view/primitive/impl/EnumPropSpinnerEditor.java
@@ -0,0 +1,184 @@
+package org.jpropeller.view.primitive.impl;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.AbstractSpinnerModel;
+import javax.swing.JComponent;
+import javax.swing.JSpinner;
+import javax.swing.JSpinner.DefaultEditor;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.jpropeller.properties.Prop;
+import org.jpropeller.properties.change.Change;
+import org.jpropeller.properties.change.Changeable;
+import org.jpropeller.system.Props;
+import org.jpropeller.view.JView;
+
+/**
+ * JSpinner that displays a Prop<Enum>
+ * @param <T> Type of enum.
+ */
+public class EnumPropSpinnerEditor<T extends Enum<T>> implements JView, org.jpropeller.properties.change.ChangeListener {
+
+	private final Prop<T> model;
+	private final T[] values;
+	private final EnumSpinnerModel spinnerModel;
+	private JSpinner spinner;
+	private final Class<T> type;
+	
+	private class EnumSpinnerModel extends AbstractSpinnerModel {
+
+		private int current = 0;
+		private T value;
+		
+		private EnumSpinnerModel() {
+			value = values[0];
+		}
+		
+		private int delta(final int delta) {
+			int next = (current+delta);
+			if(next < 0) {
+				return values.length-1;
+			} else {
+				return next % values.length;
+			}
+		}
+		
+		@Override
+		public Object getNextValue() {
+			return values[delta(1)];
+		}
+
+		@Override
+		public Object getPreviousValue() {
+			return values[delta(-1)];
+		}
+
+		@Override
+		public Object getValue() {
+			return value;
+		}
+
+		@Override
+		public void setValue(Object value) {
+			if(type.isAssignableFrom(value.getClass())) {
+				this.value = type.cast(value);
+				this.current = this.value.ordinal();
+				fireStateChanged();
+			}
+		}
+		
+	}
+
+	/**
+	 * Create a new editor.
+	 * @param type		The type of the enum.
+	 * @param values	The possible values of the enum (can't find via reflection).
+	 * @param model		The prop to edit.
+	 */
+	public EnumPropSpinnerEditor(Class<T> type, T[] values, final Prop<T> model) {
+		this.type = type;
+		this.model = model;
+		this.values = values;
+		this.spinnerModel = new EnumSpinnerModel();
+		this.spinner = new JSpinner(spinnerModel);
+		applyFocusFix(spinner);
+		spinner.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				commit();
+			}
+		});
+		model.features().addListener(this);
+		Props.getPropSystem().getUpdateManager().registerUpdatable(this);
+	}
+	
+	private void applyFocusFix(final JSpinner spinner) {
+		final JComponent editor = spinner.getEditor();
+		
+		//If the editor is a default editor, we can work around
+		//failure to commit when selecting a menu (and possibly other
+		//problems) by monitoring the editor, and committing its edits when it loses
+		//focus. This should happen automatically, and sometimes does by some
+		//means I have not yet located, but fails when immediately selecting a menu 
+		//after editing, etc.
+		if (editor instanceof DefaultEditor) {
+			final DefaultEditor dEditor = (DefaultEditor)editor;
+			dEditor.getTextField().addFocusListener(new FocusListener() {
+				
+				//On focus lost, try to commit any pending edit.
+				//If this fails, then revert editor to the actual
+				//current value to avoid confusion
+				public void focusLost(FocusEvent e) {
+					try {
+						spinner.commitEdit();
+					} catch (ParseException e1) {
+						cancel();
+					}
+				}
+				
+				//Do nothing on focus gained
+				public void focusGained(FocusEvent e) {
+				}
+			});
+		}
+	}
+	
+	@Override
+	public Format format() {
+		return Format.SINGLE_LINE;
+	}
+
+	@Override
+	public JComponent getComponent() {
+		return spinner;
+	}
+
+	@Override
+	public boolean selfNaming() {
+		return false;
+	}
+
+	@Override
+	public void cancel() {
+		//No such thing!
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void commit()  {
+		if(model.get() != spinnerModel.getValue()) {
+			model.set((T) spinnerModel.getValue());
+		}
+	}
+
+	@Override
+	public boolean isEditing() {
+		return false;
+	}
+
+	@Override
+	public void dispose() {
+		model.features().removeListener(this);
+		Props.getPropSystem().getUpdateManager().deregisterUpdatable(this);
+	}
+
+	@Override
+	public void update() {
+		if(spinnerModel.getValue() != model.get()) {
+			spinnerModel.setValue(model.get());
+		}
+	}
+
+	@Override
+	public void change(List<Changeable> initial, Map<Changeable, Change> changes) {
+		Props.getPropSystem().getUpdateManager().updateRequiredBy(this);
+	}
+	
+	
+}
---
+++ jpropeller/src/org/jpropeller/view/primitive/impl/NumberPropSpinnerEditor.java
@@ -0,0 +1,223 @@
+package org.jpropeller.view.primitive.impl;
+
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.swing.JComponent;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JSpinner.DefaultEditor;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.jpropeller.properties.Prop;
+import org.jpropeller.properties.change.Change;
+import org.jpropeller.properties.change.Changeable;
+import org.jpropeller.properties.exception.InvalidValueException;
+import org.jpropeller.properties.exception.ReadOnlyException;
+import org.jpropeller.system.Props;
+import org.jpropeller.util.GeneralUtils;
+import org.jpropeller.util.NumberConverter;
+import org.jpropeller.view.JView;
+import org.jpropeller.view.View;
+import org.jpropeller.view.Views;
+
+/**
+ *
+ * JSpinner that displays a Prop<Number>
+ * @param <T> Type of number.
+ */
+public class NumberPropSpinnerEditor<T extends Number & Comparable<T>> implements JView, org.jpropeller.properties.change.ChangeListener {
+
+	Logger logger = GeneralUtils.logger(NumberPropSpinnerEditor.class);
+	
+	private final JSpinner spinner;
+	private final SpinnerNumberModel numberModel;
+	private final NumberConverter<T> converter;
+	private final Prop<T> model;
+	
+	/**
+	 * Original default bg colour for spinner
+	 */
+	private Color defaultBackground;
+
+	/**
+	 * Create a {@link NumberPropSpinnerEditor}
+	 * @param model
+	 * 		The model for this {@link View} 
+	 * @param numberModel
+	 * 		The number model for the spinner. Please do not use
+	 * this number model except to pass it to this constructor.
+	 * @param converter 
+	 * 		Converter to move from {@link Number} to T and back
+	 */
+	public NumberPropSpinnerEditor(final Prop<T> model, 
+			SpinnerNumberModel numberModel, NumberConverter<T> converter) {
+		super();
+		this.model = model;
+		this.numberModel = numberModel;
+		this.converter = converter;
+
+		Props.getPropSystem().getUpdateManager().registerUpdatable(this);
+		model.features().addListener(this);
+		
+		//Make a spinner that will behave properly on lost focus
+		spinner = new JSpinner(numberModel);
+		defaultBackground = spinner.getBackground();
+		applyFocusFix(spinner);
+		spinner.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				commit();
+			}
+		});
+	}
+
+	@Override
+	public void dispose() {
+		model.features().removeListener(this);
+		Props.getPropSystem().getUpdateManager().deregisterUpdatable(this);
+	}
+
+	private void error(boolean errored) {
+		if (errored) {
+			spinner.setBackground(Views.getViewSystem().getErrorBackgroundColor());
+		} else {
+			spinner.setBackground(defaultBackground);
+		}
+	}
+	
+	@Override
+	public void commit() {
+		//If we are editing, set the new prop value
+		if (isEditing()) {
+			try {
+				model.set(converter.toT(numberModel.getNumber()));
+				error(false);
+			} catch (ReadOnlyException e) {
+				error(true);
+				update();
+			} catch (InvalidValueException e) {
+				error(true);
+				update();
+			}
+
+		}
+	}
+
+	private boolean checkNull(T value) {
+		boolean n = (value == null);
+		spinner.setEnabled(!n);
+		return n;
+	}
+	
+	@Override
+	public boolean isEditing() {
+		T value = model.get();
+
+		//If value is null, not editing
+		if (checkNull(value)) {
+			return false;
+		}
+		
+		//We are editing if a commit now would do something
+		return (!converter.toT(numberModel.getNumber()).equals(value));
+	}
+
+	//We need to use the raw Comparable provided by NumberModel
+	@SuppressWarnings("unchecked")
+	@Override
+	public void update() {
+		/*if (logger.isLoggable(Level.FINEST)) {
+			logger.finest("update()");
+			logger.finest("prop value " + help.getPropValue());
+			logger.finest("spinner value " + numberModel.getNumber());
+		}*/
+		
+		//If the spinner is not already showing prop value, update it
+		if (isEditing()) {
+			
+			T value = model.get();
+			
+			error(false);
+
+			//Can't display null values
+			if (checkNull(value)) {
+				return;
+			}
+			
+			//Expand the range of the number model as necessary to contain the
+			//new value
+			if (numberModel.getMinimum() != null && numberModel.getMinimum().compareTo(value) > 0){
+				numberModel.setMinimum(value);
+			}
+			if (numberModel.getMaximum() != null && numberModel.getMaximum().compareTo(value) < 0){
+				numberModel.setMaximum(value);
+			}
+			
+			spinner.setValue(converter.toNumber(value));
+		}
+	}
+	
+	private void applyFocusFix(final JSpinner spinner) {
+		final JComponent editor = spinner.getEditor();
+		
+		//If the editor is a default editor, we can work around
+		//failure to commit when selecting a menu (and possibly other
+		//problems) by monitoring the editor, and committing its edits when it loses
+		//focus. This should happen automatically, and sometimes does by some
+		//means I have not yet located, but fails when immediately selecting a menu 
+		//after editing, etc.
+		if (editor instanceof DefaultEditor) {
+			final DefaultEditor dEditor = (DefaultEditor)editor;
+			dEditor.getTextField().addFocusListener(new FocusListener() {
+				
+				//On focus lost, try to commit any pending edit.
+				//If this fails, then revert editor to the actual
+				//current value to avoid confusion
+				public void focusLost(FocusEvent e) {
+					try {
+						spinner.commitEdit();
+					} catch (ParseException e1) {
+						cancel();
+					}
+				}
+				
+				//Do nothing on focus gained
+				public void focusGained(FocusEvent e) {
+				}
+			});
+		}
+	}
+	
+	@Override
+	public boolean selfNaming() {
+		//Just displays value
+		return false;
+	}
+	
+	@Override
+	public Format format() {
+		return Format.SINGLE_LINE;
+	}
+
+	@Override
+	public JComponent getComponent() {
+		return spinner;
+	}
+
+	@Override
+	public void cancel() {
+		//??
+	}
+
+	@Override
+	public void change(List<Changeable> initial, Map<Changeable, Change> changes) {
+		Props.getPropSystem().getUpdateManager().updateRequiredBy(this);
+	}
+}
