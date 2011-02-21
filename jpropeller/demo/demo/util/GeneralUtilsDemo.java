c7bac4d38e7348a62f5a8eb4a56bf89c62e90956
trepidacious <trepidacious@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2011-02-21 17:18:39 +0000

    * Added method to try to get the best OS dependent documents folder. Tested on Windows Vista and OS X 10.6. Needs testing on other platforms, but pretty confident it will be at least better than user.home on each platform.  * Added demo of GeneralUtils

---
+++ jpropeller/demo/demo/util/GeneralUtilsDemo.java
@@ -0,0 +1,18 @@
+package demo.util;
+
+import org.jpropeller.util.GeneralUtils;
+
+/**
+ * Demonstrate some features of {@link GeneralUtils}
+ */
+public class GeneralUtilsDemo {
+
+	/**
+	 * Run demos
+	 * @param args	Ignores
+	 */
+	public static void main(String[] args) {
+		System.out.println(GeneralUtils.getDocumentsFolder());
+	}
+
+}
