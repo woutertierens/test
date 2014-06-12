fdd92dddb2c4398d839a48902ee6712b795ed37b
trepidacious <trepidacious@e3993962-97ac-11dd-8f69-eb63e1080c9a>
2014-06-12 12:30:38 +0000

    Removed unused imports.

--- jpropeller/src/org/jpropeller/util/ModifiedBasicSplitPaneUI.java
+++ jpropeller/src/org/jpropeller/util/ModifiedBasicSplitPaneUI.java
@@ -14,7 +14,6 @@ import java.awt.LayoutManager;
 import java.awt.LayoutManager2;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
